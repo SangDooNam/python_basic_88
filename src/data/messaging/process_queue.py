c6fa2f7cbb748d8daf192d8f160e1d8c5291d91d
SangDoo Nam <kexe009@hotmail.com>
2023-12-05 19:41:17 +0100

    modified

--- src/data/messaging/process_queue.py
+++ src/data/messaging/process_queue.py
@@ -4,11 +4,12 @@ import os
 import json
 import uuid
 import random
+
 # Define your constants here
 ROOT = os.path.abspath(os.getcwd())
 DATA_ROOT = os.path.dirname(ROOT)
-QUEUE_ROOT = os.path.join(ROOT, 'queue')
-USERS_ROOT = os.path.join(ROOT, 'users')
+QUEUE_ROOT = os.path.join(ROOT, "queue")
+USERS_ROOT = os.path.join(ROOT, "users")
 
 # You can use the following structure of functions or make your own.
 
@@ -16,19 +17,20 @@ USERS_ROOT = os.path.join(ROOT, 'users')
 # A notification is sent similarly to the way a massage is sent
 
 # unread directory
-# sent directory queue/sent 
+# sent directory queue/sent
 # root directory messages.log  Message {queue_file_name} processed with status {True/False}.
 # user_name directory read directory unread directory user_name/read, user_name/unread
 # this user directory will be created, if they don't exist already, right before sending a message or notification
+
 
 def parse_message(text):
     """Convert a text message into a dictionary."""
     lst = []
     message_dict = {}
-    with open(QUEUE_ROOT+'/'+text, 'r') as message:
+    with open(os.path.join(QUEUE_ROOT, text), "r") as message:
         message_list = message.readlines()
         for line in message_list:
-            splited = line.split(':')
+            splited = line.split(":")
             lst.append(splited)
         for i in lst:
             key = i[0]
@@ -36,108 +38,101 @@ def parse_message(text):
                 if key not in message_dict:
                     message_dict[key] = {}
                 message_dict[key] = j
-    
+
     return message_dict
 
 
 def create_user_directories(user):
     """Create a new user directory tree."""
-    path_user = '/' + user 
-    path_read = path_user + '/' + 'read'
-    path_unread = path_user + '/' + 'unread'
-    os.makedirs(USERS_ROOT+path_user, exist_ok=True)
-    os.makedirs(USERS_ROOT+path_read, exist_ok=True)
-    os.makedirs(USERS_ROOT+path_unread, exist_ok=True)
+    path_user = os.path.join(USERS_ROOT, user)
+    path_read = os.path.join(path_user, 'read')
+    path_unread = os.path.join(path_user, 'unread')
+    os.makedirs(path_user, exist_ok=True)
+    os.makedirs(path_read, exist_ok=True)
+    os.makedirs(path_unread, exist_ok=True)
 
 
 def create_file_name():
     """Create a unique name."""
-    file_format = '.txt'
-    file_name = str(uuid.uuid4())+file_format 
-    
+    file_format = ".txt"
+    file_name = str(uuid.uuid4()) + file_format
+
     return file_name
 
 
 def send_message(message):
     """Send a message to a user."""
+    log_path = os.path.join(ROOT, "messages.log")
     if random.random() > 0.5:
-        to = parse_message(message).get('to')
-        to = to.replace('\n', '').strip()
+        to = parse_message(message).get("to")
+        to = to.replace("\n", "").strip()
         create_user_directories(to)
-        slash = '/'
-        unread = 'unread'
+        unread = "unread"
         file_name = create_file_name()
         msg = json.dumps(parse_message(message))
         status = True
-        with open(USERS_ROOT+slash+to+slash+unread+file_name, 'w') as sending_msg:
+        with open(
+            os.path.join(USERS_ROOT, to, unread ,file_name), "w"
+        ) as sending_msg:
             sending_msg.write(msg)
-        
-        
-        with open(ROOT+'/'+'messages.log', 'a') as log:
-            log_message = f'Message {message} processed with status {status}.'
-            log.write(log_message+'\n')
+
+        with open(log_path, "a") as log:
+            log_message = f"Message {message} processed with status {status}."
+            log.write(log_message + "\n")
         notification(status, message)
         move_to_sent(message)
         return True
     else:
         status = False
-        with open(ROOT+'/'+'messages.log', 'a') as log:
-            log_message = f'Message {message} processed with status {status}.'
-            log.write(log_message+'\n')
+        with open(log_path, "a") as log:
+            log_message = f"Message {message} processed with status {status}."
+            log.write(log_message + "\n")
         notification(status, message)
         return False
 
 
 def move_to_sent(message):
     """Move the queue file to the queue sent directory."""
-    slash = '/'
-    sent = 'sent'
-    path_sent = QUEUE_ROOT+slash+sent
+    sent = "sent"
+    path_sent = os.path.join(QUEUE_ROOT, sent)
     os.makedirs(path_sent, exist_ok=True)
-    os.rename(QUEUE_ROOT+slash+message, path_sent+slash+message)
+    os.rename(os.path.join(QUEUE_ROOT, message), os.path.join(path_sent, message))
 
 
 def process_queue():
     """Take the messages in the queue and send them."""
-    send_message('1.txt')
-    send_message('2.txt')
-    send_message('3.txt')
-    send_message('4.txt')
-    send_message('5.txt')
+    send_message("1.txt")
+    send_message("2.txt")
+    send_message("3.txt")
+    send_message("4.txt")
+    send_message("5.txt")
 
 
 def notification(status, message):
     """Send notifications according to the message status."""
-    sender = parse_message(message).get('from')
-    sender = sender.replace('\n', '').strip()
-    to = parse_message(message).get('to')
-    to = to.replace('\n', '').strip()
-    title = parse_message(message).get('title')
-    title = title.replace('\n', '').strip()
-    path_ok = ROOT + '/' + 'templates' + '/' + 'ok.txt'
-    path_ko = ROOT + '/' + 'templates' + '/' + 'ko.txt'
-    path_sender = USERS_ROOT + '/' + sender + '/' + 'unread'
+    sender = parse_message(message).get("from")
+    sender = sender.replace("\n", "").strip()
+    to = parse_message(message).get("to")
+    to = to.replace("\n", "").strip()
+    title = parse_message(message).get("title")
+    title = title.replace("\n", "").strip()
+    path_ok = os.path.join(ROOT, "templates", "ok.txt")
+    path_ko = os.path.join(ROOT, "templates", "ko.txt")
+    path_sender = os.path.join(USERS_ROOT, sender, 'unread')
     file_name = create_file_name()
     create_user_directories(sender)
     if status:
-        with open(path_ok, 'r') as ok:
+        with open(path_ok, "r") as ok:
             ok_txt = ok.read()
             ok_txt = ok_txt.format(sender=sender, to=to, title=title)
-        with open(path_sender+file_name, 'w') as notifcation_ok:
+        with open(path_sender + file_name, "w") as notifcation_ok:
             notifcation_ok.write(ok_txt)
     else:
-        with open(path_ko, 'r') as ko:
+        with open(path_ko, "r") as ko:
             ko_txt = ko.read()
             ko_txt = ko_txt.format(sender=sender, to=to, title=title)
-        with open(path_sender+file_name, 'w') as notifcation_ko:
+        with open(path_sender + file_name, "w") as notifcation_ko:
             notifcation_ko.write(ko_txt)
-        
 
 
 process_queue()
-
-
-
-
-
-
